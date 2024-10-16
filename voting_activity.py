efcaebc5573a73586df255e6af60c733cb822b01
Janne Salokoski <salokoski.janne@gmail.com>
2024-10-16 14:02:58 +0300

    Make the plot nice and presentable

--- voting_activity.py
+++ voting_activity.py
@@ -2,35 +2,68 @@
 
 import api
 import pandas as pd
+import numpy as np
 
-from plotnine import ggplot, ggsave, aes, geom_smooth, geom_point, geom_line
+from plotnine import (ggplot, ggsave, aes, geom_smooth, geom_point, geom_line,
+scale_color_manual, scale_y_continuous, scale_x_continuous, labs, xlab, ylab)
 
 def main():
     years = [2018, 2020, 2022, 2024]
     frames = []
     for year in years:
         data = api.votes_by_hour(year)["children"]
+        total = data["total"]
+
+        print(total["vote_count"], total["voter_count"], total["percentage"])
+
         votes = data["votes"]
 
         df = pd.DataFrame.from_dict(votes)
-        df.insert(1, "hour", range(0, len(df)), True)
+        if year != years[-1]:
+            max_hours = 60
+            hours = len(df)
+            step = max_hours / hours
+            df.insert(1, "hour", np.arange(0, max_hours, step), True)
+        else:
+            df.insert(1, "hour", range(0, len(df)), True)
+
         df.insert(2, "year", [str(year)] * len(df), True)
         frames.append(df)
 
     df = pd.concat(frames)
+    df = df.sort_values(by="year", ascending=False)
 
-    print(df.columns)
+    colors = ["#ea76cb33", "#e6455366", "#df8e1d99", "#209fb5ff"]
 
-    plot = (ggplot(
-                data = df,
-                mapping = aes(x="hour", y="cumulative_count", colour="year")
+    last = df[df["year"] == "2024"]
+    others = df[df["year"] != "2024"]
+
+    plot = (ggplot()
+            + geom_point(data=others, mapping=aes(x="hour",
+                y="cumulative_count", color="year"))
+            + geom_line(data=others, mapping=aes(x="hour",
+                y="cumulative_count", color="year"))
+
+            + geom_point(data=last, mapping=aes(x="hour",
+                y="cumulative_count", color="year"))
+            + geom_line(data=last, mapping=aes(x="hour",
+                y="cumulative_count", color="year"))
+
+            + scale_x_continuous(breaks = range(0,60,5))
+
+            + scale_color_manual(values=colors)
+
+            + labs(
+                title = "Äänestysaktiivisuus HYYn edustajistovaaleissa",
+                subtitle = "Äänien jakautuminen äänestystunneille vuosina 2018-2024",
+                caption = "lähde: http://vaalitulos.hyy.fi",
+                color = "Vuosi"
+                )
+            + xlab("Äänestystunti")
+            + ylab("Äänimäärä")
             )
-            + geom_point()
-            # + geom_smooth()
-            + geom_line()
-            )
 
-    ggsave(plot, filename="plot.png")
+    ggsave(plot, filename="plot.png", width=10, height=5)
 
 if __name__ == "__main__":
     main()
